bd4ac241f00b7ce91c6ba07b81161d8b133690c3
DoctorDavinci <dr.dav@hotmail.com>
2018-05-12 17:03:55 -0400

    Initial Upload

---
+++ EVAChutePack_Plugin/EVAChutePack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace EVAChutePack
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class EVAChutePack : MonoBehaviour
+    {
+        public void Update()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                if (FlightGlobals.ActiveVessel.isEVA)
+                {
+                    CheckChute();
+                }
+            }
+        }
+
+        private void CheckChute()
+        {
+            foreach (Part p in FlightGlobals.ActiveVessel.Parts)
+            {
+                var EVAchute = p.vessel.FindPartModuleImplementing<ModuleEvaChute>();
+                if (EVAchute != null)
+                {
+                    if (p.vessel.Landed || p.vessel.Splashed)
+                    {
+                        EVAchute.Repack();
+                    }
+                }
+            }
+        }
+
+        private void Dummy()
+        {
+        }
+    }
+}
